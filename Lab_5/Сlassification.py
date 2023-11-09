379e754e8dd0d50805fe5f03230123c1396cd385
Misha Khapalnov <michkahap@gmail.com>
2023-11-09 22:54:30 +0200

    Make task 2 and half of task 3

--- Lab_5/Сlassification.py
+++ Lab_5/Сlassification.py
@@ -1,5 +1,10 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 file_path = 'Glass.csv'
 data = pd.read_csv(file_path)
@@ -36,3 +41,33 @@ else:
 instances_per_class = data['Type'].value_counts()
 print("Екземпляри у кожному класі:")
 print(instances_per_class)
+
+sns.pairplot(data, hue="Type")  # Побудова матриці графіків розсіювання. Опція hue="Type" дозволяє розфарбувати графіки розсіювання відповідно до класів, що допомагає візуалізувати розподіл класів.
+
+plt.show()
+
+# Розділення на вхідні та вихідні дані
+X = data.drop('Type', axis=1)  # Ознаки
+y = data['Type']  # Цільова змінна
+
+# Перетворення цільової змінної у числові мітки
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# Розділення на тренувальний та тестовий набори даних
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Ініціалізація та навчання класифікатора
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+# Прогнозування на тестових даних
+predictions = classifier.predict(X_test)
+
+# Оцінка точності моделі
+accuracy = accuracy_score(y_test, predictions)
+print(f'Точність: {accuracy:.2f}')
+
+# Виведення звіту з класифікації
+print(classification_report(y_test, predictions))
+
